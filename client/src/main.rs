cde02f24cee65f2cc999c1e6a5e6008b1d10a59d
CoderCowMoo <broskichad38@gmail.com>
2022-10-12 10:33:50 +1100

    Finished websocket server and client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,16 @@
+use tungstenite::{connect, Message};
+use url::Url;
+
 fn main() {
-    println!("Hello, world!");
+    let (mut socket, _) =
+        connect(Url::parse("ws://localhost:9001").unwrap()).expect("Can't connect to socket");
+
+    println!("Connected to socket");
+
+    socket
+        .write_message(Message::Text("Hello dumbass socket".into()))
+        .unwrap();
+
+    let msg = socket.read_message().expect("Error reading message");
+    println!("Received: {}", msg);
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,28 @@
+use std::net::TcpListener;
+use std::thread::spawn;
+use tungstenite::accept;
+
 fn main() {
-    println!("Hello, world!");
+    let server = match TcpListener::bind("127.0.0.1:9001") {
+        Ok(w) => w,
+        Err(e) => panic!("Error in binding TcpListener: {}", e),
+    };
+
+    for stream in server.incoming() {
+        // spawn a separate thread to handle each connection
+        spawn(move || {
+            let mut websocket = match accept(stream.unwrap()) {
+                Ok(o) => o,
+                Err(e) => panic!("Failure in accepting stream: {}", e),
+            };
+
+            loop {
+                let msg = websocket.read_message().unwrap();
+
+                if msg.is_binary() || msg.is_text() {
+                    websocket.write_message(msg).unwrap();
+                }
+            }
+        });
+    }
 }
